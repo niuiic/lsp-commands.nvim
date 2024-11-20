2fc772021bb2292fff58598666e60ce33d469f6f
niuiic <niuiicde@gmail.com>
2024-11-20 23:00:33 +0800

    feat: is_enabled

--- lua/lsp-commands/init.lua
+++ lua/lsp-commands/init.lua
@@ -7,6 +7,8 @@ local M = {
 ---@field run fun()
 ---@field is_enabled (fun(): boolean) | nil
 
+---@param category string
+---@param command lsp-commands.Command
 function M.register_command(category, command)
 	M._commands[category] = M._commands[category] or {}
 	table.insert(M._commands[category], command)
@@ -25,14 +27,20 @@ function M._run_command(item)
 	end
 end
 
+---@param category string | nil
 function M.run_command(category)
 	local items = {}
 	if not category then
 		for key, value in pairs(M._commands) do
-			table.insert(items, M._get_item(key, value))
+			if not value.is_enabled or value.is_enabled() then
+				table.insert(items, M._get_item(key, value))
+			end
 		end
 	else
 		items = vim.iter((M._commands[category] or {}))
+			:filter(function(command)
+				return not command.is_enabled or command.is_enabled()
+			end)
 			:map(function(item)
 				return M._get_item(category, item)
 			end)
